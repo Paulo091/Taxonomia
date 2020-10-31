ed447d75cd7bde861e8749be149553b99b31e38c
Paulo H. Dias <paulo.h.91@hotmail.com>
2020-10-31 15:43:43 -0300

    Init

---
+++ Taxonomia.Manager/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Taxonomia.Manager.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
